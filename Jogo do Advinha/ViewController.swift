83d9ab2c218f909f9038abc93901692a65d0fae9
Leonardo Figueiredo <leocassio.af@gmail.com>
2016-10-15 12:06:11 -0300

    Initial Commit

---
+++ Jogo do Advinha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jogo do Advinha
+//
+//  Created by Leonardo Figueiredo on 15/10/16.
+//  Copyright © 2016 Leonardo Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Jogo do AdvinhaTests/Jogo_do_AdvinhaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jogo_do_AdvinhaTests.swift
+//  Jogo do AdvinhaTests
+//
+//  Created by Leonardo Figueiredo on 15/10/16.
+//  Copyright © 2016 Leonardo Figueiredo. All rights reserved.
+//
+
+import XCTest
+@testable import Jogo_do_Advinha
+
+class Jogo_do_AdvinhaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
